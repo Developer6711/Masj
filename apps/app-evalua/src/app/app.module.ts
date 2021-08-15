2822c262e9239ac286f6fd8c545bda8c1614b490
msaavedraj67 <msaavedraj67@gmail.com>
2021-08-15 09:47:18 -0500

    proyecto

--- apps/app-evalua/src/app/app.module.ts
+++ apps/app-evalua/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
